59861d2deb7a207d28aba43a9bc992ae451e3c19
Calvin Spealman <ironfroggy@gmail.com>
2013-05-13 18:00:10 -0400

    Getting a basic prototype of a WriteCtrl to handle the entry flow, passed into the WriteCtrl

--- app/scripts/controllers/write.js
+++ app/scripts/controllers/write.js
@@ -2,9 +2,26 @@
 
 angular.module('journalappApp')
   .controller('WriteCtrl', function($scope) {
-
+    $scope.addEntry = function(entry_text) {
+      console.log("save: ", entry_text);
+    }
   })
-  .directive('entry', function(scope, element, attrs) {
-    element.addClass('entry');
+  .directive('write', function() {
+    return {
+      restrict: 'A',
+      link: function($scope, element, attrs) {
+        element.bind('keydown', function(e) {
+          if (e.keyCode === 13) {
+            var entry_text = element.val()
+            $scope.addEntry(entry_text);
+          }
+        });
+        element.bind('keyup', function(e) {
+          if (e.keyCode === 13) {
+            element.val('');
+          }
+        });
+      }
+    };
   })
 ;
