119a91607dfa20877c2d0c5a7d38a3adf766dae8
Calvin Spealman <ironfroggy@gmail.com>
2013-05-13 21:30:56 -0400

    Setting up a defer-loaded service to wrap the Plasmid database.

--- app/scripts/controllers/write.js
+++ app/scripts/controllers/write.js
@@ -1,11 +1,46 @@
 'use strict';
 
 angular.module('journalappApp')
-  .controller('WriteCtrl', function($scope) {
+
+  .factory('entryStorage', function() {
+    var plasmid = require('plasmid');
+    var DB = new plasmid.Database({
+      name: "journalapp",
+      schema: {
+        version: 1,
+        stores: {
+          entries: {sync: false}
+        }
+      }
+    });
+
+    var service = {};
+    var wait = new plasmid.Promise();
+
+    DB.onopensuccess = function() {
+      wait.ok(service);
+    };
+
+    service.fetch = function() {
+      return DB.stores.entries.walk(); 
+    };
+
+    return wait;
+  })
+
+  .controller('WriteCtrl', function($scope, entryStorage) {
     $scope.addEntry = function(entry_text) {
       console.log("save: ", entry_text);
     }
+
+    entryStorage.then(function(entryStorage) {
+      entryStorage.fetch()
+      .then(function(entries){
+        console.log('found ' + entries.length + ' entries');
+      });
+    });
   })
+
   .directive('write', function() {
     return {
       restrict: 'A',
